2ed48aa11e6900313f43739f5663ece0f338cc3e
Joseph Van Boxtel <dev.josephvanboxtel@gmail.com>
2020-01-20 14:31:11 -0800

    Refactor ServiceTypesService to allow a caching layer to be inserted. This should lead to removing the fetchGroup method.

--- ServicesScheduler/AttentionNeededFeed/Service/ServiceTypesService.swift
+++ ServicesScheduler/AttentionNeededFeed/Service/ServiceTypesService.swift
@@ -9,23 +9,64 @@
 import Foundation
 import PlanningCenterSwift
 
-class ServiceTypesService {
+typealias ServiceTypeService = (MServiceType.ID, @escaping Completion<MServiceType>) -> ()
+
+class SetMapService<Input: Hashable, Output: Hashable> {
     
-    let network: PCODownloadService
+    let mapping: (Input, @escaping Completion<Output>)->()
     
-    init(network: PCODownloadService) {
-        self.network = network
+    init(mapping: @escaping (Input, @escaping Completion<Output>) -> ()) {
+        self.mapping = mapping
+    }
+    
+    func fetchMapped(
+        _ inputSet: Set<Input>,
+        completion: @escaping Completion<Set<Output>>
+    ) {
+        var results = Protected(Set<Output>())
+        let group = DispatchGroup()
+        inputSet.forEach { input in
+            group.enter()
+            self.mapping(input) { result in
+                if let output = result.value {
+                    results.mutate { partialResults in
+                        partialResults.insert(output)
+                    }
+                }
+                group.leave()
+            }
+        }
+        group.notify(queue: .global()) {
+            completion(.success(results.value))
+        }
+    }
+}
+
+class ServiceTypesService: SetMapService<MServiceType.ID, MServiceType> {
+    
+    init(serviceTypeService: @escaping ServiceTypeService) {
+        super.init(mapping: serviceTypeService)
+    }
+    
+    static func serviceTypeService(using network: PCODownloadService)
+        -> (MServiceType.ID, @escaping Completion<MServiceType>) -> ()
+    {
+        return { serviceTypeID, completion in
+            let endpoint = Endpoints.services.serviceTypes[id: serviceTypeID]
+            network.basicFetch(endpoint, completion: completion)
+        }
     }
     
     func fetchServiceTypes(
         _ serviceTypeIDs: Set<MServiceType.ID>,
         completion: @escaping Completion<Set<MServiceType>>
     ) {
-        let endpoints = serviceTypeIDs.map { serviceTypeID in
-            Endpoints.services.serviceTypes[id: serviceTypeID]
-        }
-        network.fetchGroup(endpoints) { result in
-            completion(result.map { $0.asSet() })
-        }
+        fetchMapped(serviceTypeIDs, completion: completion)
+    }
+}
+
+extension ServiceTypesService {
+    convenience init(network: PCODownloadService) {
+        self.init(serviceTypeService: Self.serviceTypeService(using: network))
     }
 }
